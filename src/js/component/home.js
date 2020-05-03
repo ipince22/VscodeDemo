19c57a27e32cc55241874a11eec0a049412b025e
Daniel Ipince <59460448+ipince22@users.noreply.github.com>
2020-05-02 20:15:10 -0400

    firts

---
+++ src/js/component/home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+export function Home() {
+	return (
+		<div className="text-center mt-5">
+			<h1>Hello Danny!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button, bootstrap is working
+			</a>
+		</div>
+	);
+}
