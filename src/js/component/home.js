1cb8329be1143e0cd4f186d9c55021866a015115
Daniel Ipince <59460448+ipince22@users.noreply.github.com>
2020-05-02 20:40:12 -0400

    05.02 8y40

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,10 +2,14 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Navbar } from "./navbar.js";
 
 //create your first component
 export function Home() {
 	return (
+		<div className="container">
+			<Navbar/>
+		</div>
 		<div className="text-center mt-5">
 			<h1>Hello Danny!</h1>
 			<p>
