001a9d7d19510fcb666b774427bd54deed49f4f6
Daniel Ipince <59460448+ipince22@users.noreply.github.com>
2020-05-02 22:01:39 -0400

    02.05 collapse

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,14 +3,14 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "./navbar.js";
+import { Collapse } from "./collapse.js";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="container">
-			<Navbar/>
-		</div>
 		<div className="text-center mt-5">
+			<Navbar />
+
 			<h1>Hello Danny!</h1>
 			<p>
 				<img src={rigoImage} />
@@ -18,6 +18,7 @@ export function Home() {
 			<a href="#" className="btn btn-success">
 				If you see this green button, bootstrap is working
 			</a>
+			<Collapse />
 		</div>
 	);
 }
